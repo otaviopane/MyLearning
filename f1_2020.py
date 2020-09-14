b25fa030ae46d594f099626e4cb71b4b943e79e6
otaviopane <otavio.pane@gmail.com>
2020-09-14 04:08:06 -0300

    13/09

---
+++ f1_2020.py
@@ -0,0 +1,16 @@
+class Driver:
+
+    def __init__(self, name, last_name, number, team, nationality, points, age, champions, victories, poles, podiums, fastest):
+        print("Creating Driver: {} ... ".format(name))
+        self.name = name
+        self.last_name = last_name
+        self.number = number
+        self.team = team
+        self.nationality = nationality
+        self.points = points
+        self.age = age
+        self.champions = champions
+        self.victories = victories
+        self.poles = poles
+        self.podiums = podiums
+        self.fastest = fastest
