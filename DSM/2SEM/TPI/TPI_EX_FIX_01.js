17042cd07487e918c87021cb9c960d57c7b8e7ed
Otavio Ferraroni G Pane <otavio.pane@gmail.com>
2021-08-15 00:08:24 -0300

    Criado arquivos dos Exercicios de Fixação de TPI, Ex01 iniciado

---
+++ DSM/2SEM/TPI/TPI_EX_FIX_01.js
@@ -0,0 +1,320 @@
+Link Slide:
+     https://fatecspgov.sharepoint.com/:b:/r/sites/Section_ILP036.A.M.139.146.20212/Class%20Materials/conceitos-basicos/conceitos-basicos.pdf?csf=1&web=1&e=EfH9mb
+
+Path Códigos:
+     /home/otaviopane/Projects/MyLearning/DSM/2SEM/TPI/JS-CONCEITOS-BASICOS/js-conceitos.js
+
+Notion Link:
+     https://www.notion.so/otaviopane/JS-9c448a8f4f254f589a95ca690375526a
+
+
+//### Exercício de fixação ###
+
+//## Você é fã de vingadores? ##
+/*Desenvolva um projeto, em JavaScript, para criar objetos 
+que representem personagens do universo Marvel, 
+especificamente os vingadores. Veja o exemplo em seguida!
+
+Importante! Você devera criar objetos para todos os 
+super-heróis e seu principal rival, o Thanos.*/
+
+//## Objeto personagem ##
+const personagem = {
+     nome: "Steve Rogers",
+     codinome: "Capitão América",
+     armaPrincipal: "Escudo americano",
+     armaSecundaria: "",
+     velocidade: 85, // 0 a 100
+     forca: 75, // 0 a 100
+     resistencia: 80, // 0 a 100
+     descricao: function () {
+          return "Nome do personagem: " + this.nome + "\n"
+               + "Codinome do personagem: " + this.codinome + "\n"
+               + "Arma principal: " + this.armaPrincipal + "\n"
+               + "Arma secundaria: " + this.armaSecundaria + "\n"
+               + "Nível de força: " + this.forca + "\n"
+               + "Nível de velocidade: " + this.velocidade + "\n"
+               + "Nível de resistência: " + this.resistencia
+
+     }
+
+}
+
+//## Objetivos! ##
+/* Todos os personagens deverão ser colocados em um Array.
+No seu projeto, desenvolva um código que faça a comparação entre cada personagem, 
+ou seja, seus atributos. Utilize estruturas de repetição (ex: for) 
+para percorrer cada elemento do Array e fazer a comparação.
+
+Por fim, seu projeto deve imprimir a descrição de cada personagem 
+que esta sendo comparado e também apontar onde um personagem “vence” 
+(possui um valor maior no atributo numérico)*/
+
+//## Capitão América ##
+const capAmerica = {
+     nome: "Steve Rogers",
+     codinome: "Capitão América",
+     armaPrincipal: "Escudo americano",
+     armaSecundaria: "",
+     velocidade: 20, // 0 a 100
+     forca: 30, // 0 a 100
+     resistencia: 30, // 0 a 100
+     descricao: function () {
+          return "Nome do personagem: " + this.nome + "\n"
+               + "Codinome do personagem: " + this.codinome + "\n"
+               + "Arma principal: " + this.armaPrincipal + "\n"
+               + "Arma secundaria: " + this.armaSecundaria + "\n"
+               + "Nível de força: " + this.forca + "\n"
+               + "Nível de velocidade: " + this.velocidade + "\n"
+               + "Nível de resistência: " + this.resistencia
+     }
+}
+
+//## Homem de Ferro ##
+const homemFerro = {
+     nome: "Tony Stark",
+     codinome: "Homem de Ferro",
+     armaPrincipal: "Armadura",
+     armaSecundaria: "",
+     velocidade: 50, // 0 a 100
+     forca: 60, // 0 a 100
+     resistencia: 60, // 0 a 100
+     descricao: function () {
+          return "Nome do personagem: " + this.nome + "\n"
+               + "Codinome do personagem: " + this.codinome + "\n"
+               + "Arma principal: " + this.armaPrincipal + "\n"
+               + "Arma secundaria: " + this.armaSecundaria + "\n"
+               + "Nível de força: " + this.forca + "\n"
+               + "Nível de velocidade: " + this.velocidade + "\n"
+               + "Nível de resistência: " + this.resistencia
+     }
+}
+
+//## Thor ##
+const 000 = {
+     nome: "000",
+     codinome: "000",
+     armaPrincipal: "0",
+     armaSecundaria: "",
+     velocidade: 00, // 0 a 100
+     forca: 00, // 0 a 100
+     resistencia: 000, // 0 a 100
+     descricao: function () {
+          return "Nome do personagem: " + this.nome + "\n"
+               + "Codinome do personagem: " + this.codinome + "\n"
+               + "Arma principal: " + this.armaPrincipal + "\n"
+               + "Arma secundaria: " + this.armaSecundaria + "\n"
+               + "Nível de força: " + this.forca + "\n"
+               + "Nível de velocidade: " + this.velocidade + "\n"
+               + "Nível de resistência: " + this.resistencia
+     }
+}
+//## Hulk ##
+const 000 = {
+     nome: "000",
+     codinome: "000",
+     armaPrincipal: "0",
+     armaSecundaria: "",
+     velocidade: 00, // 0 a 100
+     forca: 00, // 0 a 100
+     resistencia: 000, // 0 a 100
+     descricao: function () {
+          return "Nome do personagem: " + this.nome + "\n"
+               + "Codinome do personagem: " + this.codinome + "\n"
+               + "Arma principal: " + this.armaPrincipal + "\n"
+               + "Arma secundaria: " + this.armaSecundaria + "\n"
+               + "Nível de força: " + this.forca + "\n"
+               + "Nível de velocidade: " + this.velocidade + "\n"
+               + "Nível de resistência: " + this.resistencia
+     }
+}
+//## Pantera Negra ##
+const 000 = {
+     nome: "000",
+     codinome: "000",
+     armaPrincipal: "0",
+     armaSecundaria: "",
+     velocidade: 00, // 0 a 100
+     forca: 00, // 0 a 100
+     resistencia: 000, // 0 a 100
+     descricao: function () {
+          return "Nome do personagem: " + this.nome + "\n"
+               + "Codinome do personagem: " + this.codinome + "\n"
+               + "Arma principal: " + this.armaPrincipal + "\n"
+               + "Arma secundaria: " + this.armaSecundaria + "\n"
+               + "Nível de força: " + this.forca + "\n"
+               + "Nível de velocidade: " + this.velocidade + "\n"
+               + "Nível de resistência: " + this.resistencia
+     }
+}
+//## Viúva Negra ##
+const 000 = {
+     nome: "000",
+     codinome: "000",
+     armaPrincipal: "0",
+     armaSecundaria: "",
+     velocidade: 00, // 0 a 100
+     forca: 00, // 0 a 100
+     resistencia: 000, // 0 a 100
+     descricao: function () {
+          return "Nome do personagem: " + this.nome + "\n"
+               + "Codinome do personagem: " + this.codinome + "\n"
+               + "Arma principal: " + this.armaPrincipal + "\n"
+               + "Arma secundaria: " + this.armaSecundaria + "\n"
+               + "Nível de força: " + this.forca + "\n"
+               + "Nível de velocidade: " + this.velocidade + "\n"
+               + "Nível de resistência: " + this.resistencia
+     }
+}
+//## Gavião Arqueiro ##
+const 000 = {
+     nome: "000",
+     codinome: "000",
+     armaPrincipal: "0",
+     armaSecundaria: "",
+     velocidade: 00, // 0 a 100
+     forca: 00, // 0 a 100
+     resistencia: 000, // 0 a 100
+     descricao: function () {
+          return "Nome do personagem: " + this.nome + "\n"
+               + "Codinome do personagem: " + this.codinome + "\n"
+               + "Arma principal: " + this.armaPrincipal + "\n"
+               + "Arma secundaria: " + this.armaSecundaria + "\n"
+               + "Nível de força: " + this.forca + "\n"
+               + "Nível de velocidade: " + this.velocidade + "\n"
+               + "Nível de resistência: " + this.resistencia
+     }
+}
+//## Homem Formiga ##
+const 000 = {
+     nome: "000",
+     codinome: "000",
+     armaPrincipal: "0",
+     armaSecundaria: "",
+     velocidade: 00, // 0 a 100
+     forca: 00, // 0 a 100
+     resistencia: 000, // 0 a 100
+     descricao: function () {
+          return "Nome do personagem: " + this.nome + "\n"
+               + "Codinome do personagem: " + this.codinome + "\n"
+               + "Arma principal: " + this.armaPrincipal + "\n"
+               + "Arma secundaria: " + this.armaSecundaria + "\n"
+               + "Nível de força: " + this.forca + "\n"
+               + "Nível de velocidade: " + this.velocidade + "\n"
+               + "Nível de resistência: " + this.resistencia
+     }
+}
+//## Homem Aranha ##
+const homemAranha = {
+     nome: "Peter Parker",
+     codinome: "Homem Aranha",
+     armaPrincipal: "Teia de Aranha",
+     armaSecundaria: "",
+     velocidade: 30, // 0 a 100
+     forca: 40, // 0 a 100
+     resistencia: 30, // 0 a 100
+     descricao: function () {
+          return "Nome do personagem: " + this.nome + "\n"
+               + "Codinome do personagem: " + this.codinome + "\n"
+               + "Arma principal: " + this.armaPrincipal + "\n"
+               + "Arma secundaria: " + this.armaSecundaria + "\n"
+               + "Nível de força: " + this.forca + "\n"
+               + "Nível de velocidade: " + this.velocidade + "\n"
+               + "Nível de resistência: " + this.resistencia
+     }
+}
+//## Dr Estranho ##
+const 000 = {
+     nome: "000",
+     codinome: "000",
+     armaPrincipal: "0",
+     armaSecundaria: "",
+     velocidade: 00, // 0 a 100
+     forca: 00, // 0 a 100
+     resistencia: 000, // 0 a 100
+     descricao: function () {
+          return "Nome do personagem: " + this.nome + "\n"
+               + "Codinome do personagem: " + this.codinome + "\n"
+               + "Arma principal: " + this.armaPrincipal + "\n"
+               + "Arma secundaria: " + this.armaSecundaria + "\n"
+               + "Nível de força: " + this.forca + "\n"
+               + "Nível de velocidade: " + this.velocidade + "\n"
+               + "Nível de resistência: " + this.resistencia
+     }
+}
+//## Feiticeira Escarlate ##
+const 000 = {
+     nome: "000",
+     codinome: "000",
+     armaPrincipal: "0",
+     armaSecundaria: "",
+     velocidade: 00, // 0 a 100
+     forca: 00, // 0 a 100
+     resistencia: 000, // 0 a 100
+     descricao: function () {
+          return "Nome do personagem: " + this.nome + "\n"
+               + "Codinome do personagem: " + this.codinome + "\n"
+               + "Arma principal: " + this.armaPrincipal + "\n"
+               + "Arma secundaria: " + this.armaSecundaria + "\n"
+               + "Nível de força: " + this.forca + "\n"
+               + "Nível de velocidade: " + this.velocidade + "\n"
+               + "Nível de resistência: " + this.resistencia
+     }
+}
+//## Visão ##
+const 000 = {
+     nome: "000",
+     codinome: "000",
+     armaPrincipal: "0",
+     armaSecundaria: "",
+     velocidade: 00, // 0 a 100
+     forca: 00, // 0 a 100
+     resistencia: 000, // 0 a 100
+     descricao: function () {
+          return "Nome do personagem: " + this.nome + "\n"
+               + "Codinome do personagem: " + this.codinome + "\n"
+               + "Arma principal: " + this.armaPrincipal + "\n"
+               + "Arma secundaria: " + this.armaSecundaria + "\n"
+               + "Nível de força: " + this.forca + "\n"
+               + "Nível de velocidade: " + this.velocidade + "\n"
+               + "Nível de resistência: " + this.resistencia
+     }
+}
+//## Peter Quill / Senhor das Estrelas ##
+const 000 = {
+     nome: "000",
+     codinome: "000",
+     armaPrincipal: "0",
+     armaSecundaria: "",
+     velocidade: 00, // 0 a 100
+     forca: 00, // 0 a 100
+     resistencia: 000, // 0 a 100
+     descricao: function () {
+          return "Nome do personagem: " + this.nome + "\n"
+               + "Codinome do personagem: " + this.codinome + "\n"
+               + "Arma principal: " + this.armaPrincipal + "\n"
+               + "Arma secundaria: " + this.armaSecundaria + "\n"
+               + "Nível de força: " + this.forca + "\n"
+               + "Nível de velocidade: " + this.velocidade + "\n"
+               + "Nível de resistência: " + this.resistencia
+     }
+}
+//## Thanos ##
+const 000 = {
+     nome: "000",
+     codinome: "000",
+     armaPrincipal: "0",
+     armaSecundaria: "",
+     velocidade: 00, // 0 a 100
+     forca: 00, // 0 a 100
+     resistencia: 000, // 0 a 100
+     descricao: function () {
+          return "Nome do personagem: " + this.nome + "\n"
+               + "Codinome do personagem: " + this.codinome + "\n"
+               + "Arma principal: " + this.armaPrincipal + "\n"
+               + "Arma secundaria: " + this.armaSecundaria + "\n"
+               + "Nível de força: " + this.forca + "\n"
+               + "Nível de velocidade: " + this.velocidade + "\n"
+               + "Nível de resistência: " + this.resistencia
+     }
+}
