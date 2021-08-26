6d8d4f55dd59d08e08fdc67eef272b44bb2e57c5
Otavio Ferraroni G Pane <otavio.pane@gmail.com>
2021-08-25 22:02:07 -0300

    Finalizado estudo de Estruturas de repetiçao e de dados dos pdfs de TPI

--- DSM/2SEM/TPI/20210800.js
+++ DSM/2SEM/TPI/20210800.js
@@ -1,4 +1,13 @@
 /*--Estruturas de repetição--*/
+let removerElemento = (numeros, elemento) => {
+  for (var indice = 0; indice < numeros.length; indice++) {
+    // A condição de parada aparece no início do loop
+    if (numeros[indice] === elemento) {
+      numeros.splice(indice, 1);
+    }
+  }
+};
+
 let sortear = (possibilidades) => {
   let numeroSorteado = Math.floor(Math.random() * possibilidades);
   return numeroSorteado;
@@ -24,4 +33,90 @@ let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 let resultado = sorteio(numeros, 3);
 
 console.log(resultado);
+
+/*--For in--*/
+
+let empresa = { razaoSocial: "ABC LTDA", nomeFantasia: "Mercado Online" };
+
+for (valor in empresa) {
+  console.log(valor);
+}
+
+/*--For in--*/
+
+let estadosSudeste = new Set(["ES", "SP", "MG", "RJ"]);
+
+for (let valores of estadosSudeste) {
+  console.log(valores);
+}
+
+/*--Estruturas de dados--*/
+
+/*--Map--*/
+
+let estrutura = new Map();
+
+estrutura.set(1, "12");
+
+estrutura.set("funcao", function () {
+  return "função anônima dentro do map";
+});
+
+console.log(estrutura.get(1));
+
+console.log(estrutura.get("funcao")());
+
 /*---*/
+
+let estrutura = new Map();
+
+estrutura.set(1, "12");
+
+estrutura.set(1, "120");
+
+console.log(estrutura.get(1));
+
+/*---*/
+
+let estrutura = new Map();
+
+estrutura.set(1, "120");
+
+estrutura.set(2, "120");
+
+console.log(estrutura.get(1));
+console.log(estrutura.get(2));
+
+/*---*/
+
+let estrutura = new Map();
+estrutura.set(1, "120");
+estrutura.set(2, "120");
+
+console.log("tamanho da estrutura: " + estrutura.size);
+console.log(estrutura.keys());
+console.log(estrutura.values());
+
+/*---*/
+
+let estrutura = new Map();
+estrutura.set(1, "120");
+estrutura.set(2, "120");
+
+console.log("tamanho da estrutura: " + estrutura.size);
+estrutura.delete(1);
+console.log("tamanho da estrutura: " + estrutura.size);
+estrutura.clear();
+console.log("tamanho da estrutura: " + estrutura.size);
+
+/*--Set--*/
+
+let estrutura = new Set();
+
+estrutura.add(1);
+estrutura.add(function () {
+  return "função dentro do set";
+});
+estrutura.add("1");
+
+console.log(estrutura.size);
