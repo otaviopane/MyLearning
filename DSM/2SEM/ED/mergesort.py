4e7b2b97a398dd565940d27c22beda5b51ff5908
Otavio Ferraroni G Pane <otavio.pane@gmail.com>
2021-09-28 00:24:44 -0300

    Avançando na prova de MC Camadas Alfabeticas

---
+++ DSM/2SEM/ED/mergesort.py
@@ -0,0 +1,16 @@
+import random
+array = [random.randint(0, 2000) for n in range(2000)]
+
+
+def merge(e, d)
+
+
+r = []
+
+
+copy_array = array[:]
+
+sort(array)
+print(array)
+
+assert array == sorted(copy_array)
--- DSM/2SEM/MC/camadas_alfa.2.py
+++ DSM/2SEM/MC/camadas_alfa.2.py
@@ -7,15 +7,9 @@ matriz = []
 lista = []
 letra = alfabeto[N-1]
 letras_centrais = (letra+' ') * N
-for _ in range(0, linhas):
-    matriz.append(lista)
 
-for e1 in range(0, linhas):
-    letra = alfabeto[N-1]
-    if e1 == N:
-        matriz[e1].append(f'{letras_centrais}* {letras_centrais}')
-    else:
-        matriz[e1].append((f'{letra} ')*linhas)
 
-for n in range(0, linhas):
-    print(lista[n])
+for linha in range(0, linhas):
+    if linha == N:
+        print(f'{letras_centrais}* {letras_centrais}')
+    elif linha == N-1
