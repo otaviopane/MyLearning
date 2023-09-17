64c6e7adb637f03675c897a0c43efc45649630c6
Otavio Pane <otavio.pane@gmail.com>
2023-09-17 21:41:18 +1000

    Creating Post

--- Kent/Term3/myBooks/node-str/src/app.js
+++ Kent/Term3/myBooks/node-str/src/app.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -11,6 +15,12 @@ const route = router.get('/', (req, res, next) => {
         version: "0.0.1"
     });
 });
+
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
 app.use('/', route);
+app.use('/products', create);
 
 module.exports = app;
